400adc36b45f6c1976ca956e5e295c98dbf70f4b
Macarena <macasensio@gmail.com>
2022-10-02 09:55:09 +0200

    desafío clase 8 - API RESTFull

---
+++ clase08-API-RESTful/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const { Router } = express
+const Productos = require('./api/productos.js')
+
+const productosApi = new Productos()
+const productosRouter = new Router()
+
+//middlewares
+const app = express()
+productosRouter.use(express.json())
+productosRouter.use(express.urlencoded({extended: true}))
+app.use(express.static('./public'))
+
+
+//rutas
+app.use('/api/productos', productosRouter)
+app.get('/api/productos', productosRouter)
+app.post('/api/productos', productosRouter)
+app.put('/api/productos', productosRouter)
+app.delete('/api/productos', productosRouter)
+
+//mostrar todos los productos
+productosRouter.get('/', (req, res) => {
+    res.json(productosApi.listarTodos())
+})
+
+//mostrar un producto por su id
+productosRouter.get('/:id', async (req, res) => {
+    res.status(200).json(productosApi.listarProducto(req.params.id))
+})
+
+//agregar un producto
+productosRouter.post('/', (req, res) => {
+    res.status(200).json(productosApi.guardar(req.body))
+})
+
+//modificar un producto
+productosRouter.put('/:id', (req, res) => {
+    res.status(200).json(productosApi.actualizar(req.body, req.params.id))
+})
+
+//eliminar un producto
+productosRouter.delete('/:id', (req, res) => {    
+    res.status(200).json(productosApi.eliminar(req.params.id))
+})
+
+
+//server
+const PORT = 8080
+const server = app.listen(PORT, () => console.log(`Server funcionando en el puerto ${server.address().port}`))
+server.on('error',(err) => console.log('Error --> ' + err))
