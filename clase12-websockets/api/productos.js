b07fde30b9b5cc24eae02446bb31a078a0de34d3
Macarena <macasensio@gmail.com>
2022-10-07 22:34:55 +0200

    iniciando entregable 6 - websocket

---
+++ clase12-websockets/api/productos.js
@@ -0,0 +1,47 @@
+class Productos {
+    constructor(){        
+        this.productos = []
+        this.id = 0
+    }
+
+
+    // ---------- métodos privados ----------//
+    #chequearArray(array) {
+        if (array.length > 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // ---------- métodos públicos ----------//
+    listarTodos() {
+        try {
+            if (this.#chequearArray(this.productos)) {                
+                return this.productos
+            } else {
+                return {mensaje: 'No hay productos'}
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    guardar(prod) {
+        try {
+            if (prod) {
+                const nuevoProducto = {...prod, id: ++this.id}
+                this.productos.push(nuevoProducto)
+                const nuevoArray = this.productos
+                return nuevoArray
+            } else { 
+                return ({mensaje: 'faltan datos'})
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }    
+}
+
+module.exports = Productos
